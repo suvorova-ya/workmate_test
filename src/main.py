2fe07a824386c2fbefab6ace187b65842c0da561
Yana <suvorova_ya@outlook.com>
2025-11-07 10:36:46 +0300

    Initial commit: Workmate test project

--- src/main.py
+++ src/main.py
@@ -3,36 +3,42 @@ import csv
 from statistics import fmean
 from tabulate import tabulate
 
-parser = argparse.ArgumentParser("parser csv files")
-parser.add_argument("--files", nargs="+", required=True, help="csv files can be miltiple files" )
-parser.add_argument("--report", required=True, help="report name average-rating.csv")
+def parser_csv_report():
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser("parser csv files")
+    parser.add_argument("--files", nargs="+", required=True, help="csv files can be miltiple files")
+    parser.add_argument("--report", required=True, help="report name average-rating.csv")
 
-report = {}
-for filename in args.files:
+    args = parser.parse_args()
+
+    report = {}
+    for filename in args.files:
+        try:
+            with open(filename, encoding="utf-8") as file:
+                table = csv.DictReader(file)
+                for row in table:
+                    report.setdefault(row['brand'], []).append(float(row['rating']))
+        except FileNotFoundError:
+            print("File not find")
+
+    report.update({brand: round(fmean(rating), 2) for brand, rating in report.items()})
+    output_report = args.report if args.report.endswith('csv') else args.report + ".csv"
+
+    # Записываем отсортированный отчет
+    with open(output_report, 'w', encoding="utf-8", newline="") as f:
+        output_file = csv.writer(f)
+        for brand, rating in sorted(report.items()):
+            output_file.writerow([brand, rating])
+
+    # выводим отчет в консоль
     try:
-        with open(filename, encoding="utf-8") as file:
-            table = csv.DictReader(file)
-            for row in table:
-                report.setdefault(row['brand'],[]).append(float(row['rating']))
+        with open("average-rating.csv", encoding="utf-8") as f:
+            report = csv.reader(f)
+            print(tabulate(report, headers=["brand", "rating"], tablefmt="pretty"))
     except FileNotFoundError:
-        print("File not find")
+        print("File 'average-rating.csv' not find")
 
 
-report.update({brand: round(fmean(rating),2) for brand, rating in report.items()})
-output_report = args.report if args.report.endswith('csv') else args.report + ".csv"
 
-#Записываем отсортированный отчет
-with open(output_report, 'w', encoding="utf-8", newline="") as f:
-    output_file = csv.writer(f)
-    for brand, rating in sorted(report.items()):
-        output_file.writerow([brand, rating])
-
-#выводим отчет в консоль
-try:
-    with open("average-rating.csv", encoding="utf-8") as f:
-        report = csv.reader(f)
-        print(tabulate(report, headers=["brand", "rating"], tablefmt="pretty"))
-except FileNotFoundError:
-    print("File 'average-rating.csv' not find")
+if __name__ == "__main__":
+    parser_csv_report()
